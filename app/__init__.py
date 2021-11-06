8fb0f85c08af6cc0624721c338704171606dedec
monginadiana <diana.ondieki@student.moringa.com>
2021-11-06 19:51:11 +0300

    initializing the flask extension

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # Creating the app configurations
+    # Creation of the app configurations
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
